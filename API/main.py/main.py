06a2892fe628ac81cd4f99e21958a13314e1cb6b
Juan Antonio Cel Vazquez <juanantoniocelvazquez@gmail.com>
2023-12-04 14:42:06 -0600

    Update main.py

--- API/main.py/main.py
+++ API/main.py/main.py
@@ -1,88 +1,48 @@
-from flask import Flask, request, render_template, send_from_directory
-import os
-from clases import Cleaner, LinearRegressionModel
-from werkzeug.utils import secure_filename
-import pandas as pd
+import flask as fl
 
+app = fl.Flask(__name__)
 
-model = None
+@app.route("/")
+def root():
+    return fl.render_template("tu_nombre_de_archivo_html.html")
 
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'archivos'
+@app.route("/upload", methods=['POST'])
+def upload_file():
+    # Aquí deberías procesar el archivo cargado
+    # Puedes obtener el archivo usando fl.request.files['file']
+    # Realiza la lógica de procesamiento del archivo aquí
+    save_result = "Archivo guardado exitosamente"  # Reemplaza esto con tu lógica real
+    return fl.render_template('tu_nombre_de_archivo_html.html', save=save_result)
 
-@app.route('/')
-def upload():
-    return render_template("index.html")
+@app.route("/clean")
+def clean_data():
+    # Aquí deberías realizar la limpieza de datos
+    # Reemplaza esto con tu lógica real
+    clean_result = "Datos limpiados exitosamente"
+    return fl.render_template('tu_nombre_de_archivo_html.html', clean=clean_result)
 
-@app.route('/upload', methods=['POST'])
-def upload_file_post():
-    file = request.files['file']
-    if file.filename == '':
-        return 'No file in the bucket'
-    if file.filename.split('.')[-1] != 'csv':
-        return 'The file must be a csv file'
-    else:
-        global model
-        filename = secure_filename(file.filename)
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(file_path)
-        model = file_path  # Update the global variable with file path
-        return render_template('index.html', uploaded='Archivo subido')
+@app.route("/feed")
+def train_model():
+    # Aquí deberías realizar el entrenamiento del modelo
+    # Reemplaza esto con tu lógica real
+    training_result = "El modelo ha sido entrenado exitosamente"
+    return fl.render_template('tu_nombre_de_archivo_html.html', training=training_result)
 
-model = model
+@app.route("/predict", methods=['POST'])
+def predict():
+    # Aquí deberías manejar la predicción
+    # Reemplaza esto con tu lógica real
+    net_rating_prediction = "Aquí va la predicción del Net Rating"
+    return fl.render_template('tu_nombre_de_archivo_html.html', NetRatingPrediction=net_rating_prediction)
 
-@app.route('/clean')
-def clean_and_show():
-    global model
-    if model != None:
-        cleaned_file = Cleaner(file_path=model).clean()
-        model = cleaned_file
-        return render_template('index.html', cleaned_file='archivo limpio')
-    else:
-        return render_template('index.html', cleaned_file='no hay archivos')
+@app.route("/download")
+def download():
+    # Aquí deberías manejar la descarga
+    # Reemplaza esto con tu lógica real
+    return fl.send_from_directory(app.config['UPLOAD_FOLDER'], 'output.txt', as_attachment=True)
 
-model = model
-
-@app.route('/feed')
-def feed_the_model():
-    global model
-    model = LinearRegressionModel(data=model)
-    model.perform_regression()
-    return render_template('index.html', feed_model='El modelo ha sido entrenado')
-
-model = model
-
-@app.route('/predict', methods=['POST'])
-def prediction():
-    global model  # Assuming you have initialized the model somewhere in your code
-
-    if model:
-        pricem2 = request.form.get('pricem2', 'test')
-        surface = request.form.get('surface', 'test')
-        covered = request.form.get('covered', 'test')
-        lat = request.form.get('lat', 'test')
-        lon = request.form.get('lon', 'test')
-
-    # You need to handle the case where model is not an instance of LinearRegressionModel
-        user_predict = pd.DataFrame({'price_m2': [pricem2], 'surface': [surface], 'covered': [covered], 'lat': [lat], 'lon': [lon]})
-        user_prediction = model.predict_user_input(user_predict)
-
-        model.results()
-
-        return render_template('index.html', userprediction=f'The house price would be: {user_prediction:,.2f} pesos') 
-    else:
-        return render_template('index.html', userprediction='TNo hay modelo para entranar') 
-    
-
-model = model
-
-@app.route('/download')
-
-def descarga():
-    return send_from_directory(app.config['UPLOAD_FOLDER'], 'output.txt', as_attachment=True)
-
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
